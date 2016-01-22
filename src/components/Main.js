c7f8877224c1cd72ffcb3ab0aabde6b7041a0338
Vincent Jo <chiwoojo@gmail.com>
2016-01-22 13:19:26 -0800

    minor fixes

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,9 +11,8 @@ import React from 'react';
 const Main = ({history, children}) => {
   return (
     //TODO: Delphine's Navbar here.
-    <navbar />
-    
+    <div>Hello World</div>
   )
 }
 
-export default Hello
+export default Main

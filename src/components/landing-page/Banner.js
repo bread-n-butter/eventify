3fe6c6447bb740d8d09406eeddba29276d5844ef
Vincent Jo <chiwoojo@gmail.com>
2016-01-22 15:43:39 -0800

    Created new folder to organize landing page items

---
+++ src/components/landing-page/Banner.js
@@ -0,0 +1,22 @@
+/**
+ *    Front landing page with Picture Banner,
+ *    SearchBar,
+ *    and Featured list of Events
+ *    
+ *    TODO: Vincent 
+ */
+import React from 'react'
+
+class Banner extends React.Component {
+  
+  render() {
+    return 
+    
+  }
+}
+
+export default Banner
+
+
+
+// http://wallpaperstock.net/mountains-green-fields--sky-wallpapers_47567_1280x800.jpg

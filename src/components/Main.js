1e7b76d7e3efd6ced5a927c84b9c9b7564c3a413
Vincent Jo <chiwoojo@gmail.com>
2016-01-22 12:41:55 -0800

    Added template for landing page and nav

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+/**
+ *    Template for landing page as of now,
+ *    which will contain the NAV bar
+ *    
+ *    Might extend to templating for the Dashboard.
+ *    
+ */
+
+import React from 'react';
+
+const Main = ({history, children}) => {
+  return (
+    //TODO: Delphine's Navbar here.
+    <navbar />
+    
+  )
+}
+
+export default Hello

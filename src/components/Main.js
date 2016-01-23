79f8d8c29a82e2cb167f00e1b9c4d6483c26f4f4
Vincent Jo <chiwoojo@gmail.com>
2016-01-22 18:33:23 -0800

    Added Card to front page

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,18 +9,36 @@ import React from 'react';
 import NavBar from './navBar/navBar';
 
 import Banner from './landing-page/Banner';
-import featuredEvents from './landing-page/featuredEvents'
+import EventList from './landing-page/FeatEvents';
 
+class Main extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      events : [{title: "Event1", desc: "cool", img: "http://lorempixel.com/640/480/abstract"}, 
+                {title: "Event2", desc: "awesome", img: "http://lorempixel.com/640/480/abstract" }, 
+                {title: "Event3", desc: "nice", img: "http://lorempixel.com/640/480/abstract" },
+                {title: "Event4", desc: "nice", img: "http://lorempixel.com/640/480/abstract" },
+                {title: "Event5", desc: "nice", img: "http://lorempixel.com/640/480/abstract" },
+                {title: "Event6", desc: "nice", img: "http://lorempixel.com/640/480/abstract" }]
+    }
+  }
+  
+  render() {
+    return (
+        //TODO: Delphine's Navbar here.
+      <div>
+        <div className="row">
+          <Banner /> 
+        </div>
+        <div className="container">
+          <EventList events={this.state.events} />
+        </div>
+      </div>
+    )
+  }
 
-const Main = ({history, children}) => {
-  return (
-    //TODO: Delphine's Navbar here.
-    <div>
-      <Navbar />
-      <Banner /> 
-      <featuredEvents />
-    </div>
-  )
 }
 
 export default Main;

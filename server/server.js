ae5b80ad35721b1c2b744751379207f4f8cbf3e4
Delphine Foo-Matkin <delphinef@gmail.com>
2016-01-23 12:20:24 -0800

    Required Passport. Starting to build out server-side auth.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var path = require('path');
 var webpack = require('webpack');
-var port = process.env.PORT || 8080;
 //var mysql = require('mysql');
 
+var port = process.env.PORT || 8080;
 var app = express();
-
 var isDevelopment = (process.env.NODE_ENV !== 'production');
-console.log("IsDevelopment is ", isDevelopment);
+console.log('IsDevelopment is ', isDevelopment);
 console.log(process.env.NODE_ENV);
 
 var static_path = path.join(__dirname, '../');
@@ -15,22 +14,22 @@ var static_path = path.join(__dirname, '../');
 require('./middleware.js')(app, express);
 
 app.use(express.static(static_path))
-.get('/', function(req, res) {
-  res.sendFile('index.html', {
-    root: static_path
-  });
-}).listen(port, function(err) {
-  if (err) {
-    console.log(error);
-  } else {
-    console.log('listening at ' + port);
-  }
-});
+   .get('/', function(req, res) {
+     res.sendFile('index.html', {
+       root: static_path
+     });
+   })
+   .listen(port, function(err) {
+     if (err) {
+       console.log(err);
+     } else {
+       console.log('listening at ' + port);
+     }
+   });
 
 if (isDevelopment) {
   var WebpackDevServer = require('webpack-dev-server');
   var config = require('../webpack.config');
-  var webpackDevServer = require('webpack-dev-server');
 
   new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,

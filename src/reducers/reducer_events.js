531bae7268fa61384932bf20d7a6870cea25e6ba
Vincent Jo <chiwoojo@gmail.com>
2016-01-28 19:40:54 -0800

    Added reducer function for fetching one event

--- src/reducers/reducer_events.js
+++ src/reducers/reducer_events.js
@@ -1,18 +1,30 @@
-import { FETCH_EVENTS, AUTH, LOGOUT } from '../actions/index';
 
-const INITIAL_STATE = {all: [], event: null, isLoggedin: false};
+import { FETCH_EVENTS, AUTH, LOGOUT } from '../actions/index';
+import { FETCH_EVENTS } from '../actions/index';
+import { FETCH_ONE_EVENT } from '../actions/index';
+
+const INITIAL_STATE = {
+  all: [], 
+  event: null, 
+  isLoggedIn: false,
+  eventEdit: null
+};
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
-    
+  
   case FETCH_EVENTS:
     return {...state, all: action.payload};
   case AUTH:
     return {...state, isLoggedin: action.payload.data.isLoggedin};
   case LOGOUT:
     return {...state, isLoggedin: false };
+  case FETCH_ONE_EVENT:
+    return {...state, eventEdit: action.payload};
+   
   default:
     return state;
+  
+  }     
     
-  }
 }

f871c7eabe9f937125dcc9c90eff2397fd913d3a
darwin <oleg.umarov@gmail.com>
2016-01-29 13:17:59 -0600

    progess

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -9,8 +9,13 @@ import IconButton from 'material-ui/lib/icon-button';
 import StarBorder from '../../../node_modules/material-ui/lib/svg-icons/toggle/star-border.js';
 import CircularProgress from 'material-ui/lib/circular-progress';
 
+<<<<<<< HEAD
 import NavBar from '../navBar/navBar.js'
 import {fetchEvents, auth} from '../../actions/';
+=======
+import NavBar from '../navBar/navBar.js';
+import {fetchEvents} from '../../actions/';
+>>>>>>> progess
 import Helper from '../../helpers/helpers';
 
 const styles = {
@@ -58,7 +63,6 @@ class Dashboard extends Component {
     if (!events) {
       return (
         <div>
-          <NavBar />
           <div className='center-align'>
             <CircularProgress size={2} />
           </div>

df06897dd019b997e4f3d599efbf542d504bef64
Delphine Foo-Matkin <delphinef@gmail.com>
2016-02-03 11:17:34 -0800

    Hooked up joined events list to link to event detail page on click.

--- src/components/dashboard/JoinedEventsList.js
+++ src/components/dashboard/JoinedEventsList.js
@@ -1,28 +1,37 @@
 /**
  *    Joined Events Component
- *    
+ *
  */
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 //Components
 import SmallEventCards from './SmallEventCards';
 
 export default class JoinedEventsList extends Component {
 
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  handleClick(event) {
+    this.props.select(event);
+    this.context.router.push('/event');
+  }
+
   render() {
     return (
       <ul className='collection'>
-      
+
         {
           this.props.data.map((event, index) => {
-            return <SmallEventCards key={index} event={event} />;
+            return <SmallEventCards key={index} event={event} onClick={this.handleClick.bind(this, event)}/>;
           })
         }
-        
+
       </ul>
     );
   }
-  
+
 }
 

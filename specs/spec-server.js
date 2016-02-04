9f0e8a1d6c55b6c6e5a155ba27ee6986f662fb65
Kristen Haydel <kristenhaydel@gmail.com>
2016-02-04 10:55:23 -0600

    Made changes to dummy data

--- specs/spec-server.js
+++ specs/spec-server.js
@@ -6,17 +6,39 @@ var request = require('supertest');
 var User = require('../server/config/models/user');
 var Event = require('../server/config/models/event');
 
-var app = require('../server/server.js').app;
+var data = require('./testData.json');
+var testUser = data.users[0];
+var testEvent = data.events[0];
+var newUser = data.newUser; 
+
+var server = require('../server/server.js');
 var route = {
       users: '/api/users',
       events: '/api/events'
     };
 
-describe('Event Controller', function() {
+describe('User Controller', function() {
 
+  describe('Fetching all Users', function(){
+    var users; 
+    
+    before(function(done){
+      request(server.app)
+      .get(route.users)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res){
+        users = res.body.data;
+        done();
+      });
+    });  
 
-  describe('addEvent', function(){
-    it('returns status 200', function(){});
+    it('Should return an array of users', function(){
+      expect(users).to.be.an('array');
+      expect(users).to.not.be.empty;
+      expect(users[0]).to.have.property('first_name', testUser.first_name);
+    })
+
   });
 
 

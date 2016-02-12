e4cea5d03cf99945cd6dcc30bc797dfb4559a6a6
Vincent Jo <chiwoojo@gmail.com>
2016-02-11 19:12:24 -0800

    Added documentation to dashboard

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,23 +1,20 @@
 import React, { Component, PropTypes } from 'react';
+
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { take } from 'lodash';
-
 import { updateUserLocation, fetchCreatedEvents, fetchJoinedEvents, fetchEvents, fetchOneEvent, auth, selectEvent, updateRadius } from '../../actions/';
 
-import Spinner from '../../helpers/spinner.js';
+import { take } from 'lodash';
 
-import GMapsSearchBox from '../../helpers/GMapsSearchBox';
+//Components
+import Spinner from '../../helpers/spinner.js';
 
 import FeaturedEventsList from './FeaturedEventsList';
 import JoinedEventsList from './JoinedEventsList';
 import CreatedEventsList from './CreatedEventsList';
 
 import GoogleMapsSearchBar from '../searchbar/GoogleMapsSearchBar';
-
-//experimental
 import GoogleMapsWithSearchBox from './GoogleMapsWithSearchBox';
-//endof experimental
 
 class Dashboard extends Component {
 
@@ -26,6 +23,9 @@ class Dashboard extends Component {
   };
 
   componentWillMount() {
+    //Check for autorization when User visits the Dashboard
+    //and Grabs all the events needed to populate Dashboard
+    //redirect them to '/' when not authorized
     this.props.auth().then(() => {
       if(!this.props.isLoggedIn) {
         this.context.router.push('/');
@@ -33,12 +33,12 @@ class Dashboard extends Component {
       this.props.fetchEvents();
       this.props.fetchJoinedEvents(this.props.user.id);
       this.props.fetchCreatedEvents(this.props.user.id);
-      // this.props.getProfilePic()
     });
   }
-
-  //Ask for User's Lat / Long and save that to the Redux State
+  
   componentDidMount() {
+    //Get user's current location through Google Chrome's navigator API and update User's location
+    //when the Component mounts
     let startPos;
     let that = this;
     const geoSuccess = function(position) {
@@ -60,7 +60,6 @@ class Dashboard extends Component {
     });
   }
 
-
   render() {
     const events = this.props.events;
     const joinedEvents = this.props.joinedEvents;
@@ -71,6 +70,7 @@ class Dashboard extends Component {
         <Spinner />
       );
     }
+    
     return (
       
       <div className="dashboard">
@@ -81,26 +81,32 @@ class Dashboard extends Component {
         </div>
         
         <div className="row">
+        
           <div className="col s8">
             <h3 style={{fontWeight: 600, paddingLeft: '0.60rem'}}>Featured events</h3>
             <FeaturedEventsList select={this.props.fetchOneEvent} radius={10000} data={events} user={this.props.user} updateRadius={this.props.updateRadius} />
           </div>
+          
           <div className="col s4">
-        
+          
             <div className='row' style={{height: '400px'}}>
               <GoogleMapsWithSearchBox select={this.props.fetchOneEvent} events={events} loc={this.props.user.loc} radius={this.props.user.radius}/>
             </div>
-              
+            
             <div>
               <h5 style={{paddingTop: '1.2rem'}}>Events You've Joined</h5>
               <JoinedEventsList select={this.props.fetchOneEvent} data={ take(joinedEvents, 10) } />
             </div>
-            <div className="">
+            
+            <div>
               <h5>Events You've Created</h5>
               <CreatedEventsList select={this.props.fetchOneEvent} data={ take(createdEvents, 10) } />
             </div>
+            
           </div>
+          
         </div>
+        
       </div>
     );
   }

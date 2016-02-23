8888a87d2bf99f12b0e68e4067549a0f3fcfd0c2
Vincent Jo <chiwoojo@gmail.com>
2016-02-22 20:08:12 -0800

    Added menu hamburger on collapse. WIP

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -11,19 +11,28 @@ import { bindActionCreators } from 'redux';
 import { auth } from '../../redux/actions/index';
 import { Link } from 'react-router';
 
-//Material UI component imports
+//Material UI 
 import SignupModal from '../auth/SignupModal';
 import SigninModal from '../auth/SigninModal';
 import LogoutBtn from '../auth/LogoutBtn';
 import CreateEventBtn from '../create-event/CreateEventBtn';
 import FlatButton from 'material-ui/lib/flat-button';
-
+import LeftNav from 'material-ui/lib/left-nav';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 class NavBar extends Component {
   
   static contextTypes = {
     router: PropTypes.object
   };
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      leftNav: false
+    };
+  }
 
   componentWillMount() {
     this.props.auth();
@@ -55,18 +64,34 @@ class NavBar extends Component {
         </div>
       );
     }
+    
+    //               <a onClick={this.setState({leftNav: {open: !this.state.leftNav.open}})} class="button-collapse"><i class="material-icons">menu</i></a>
 
     return (
-      <div className='navbar-fixed'>
-        <nav role="navigation" style={{position: 'fixed' , zIndex: '100'}}>
-          <div className="nav-wrapper">
-            <a href="#" className="brand-logo">Eventify</a>
+      <div>
+        <div className='navbar-fixed'>
+          <nav role="navigation" style={{position: 'fixed' , zIndex: '100'}}>
+            <div className="nav-wrapper">
+              <a href="#" className="brand-logo">Eventify</a>
+              
               <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li> <SignupModal /></li>
                 <li> <SigninModal /></li>
               </ul>
-          </div>
-        </nav>
+              
+              <ul id="nav-mobile" className="right hide-on-med-and-up">
+                <li> 
+                  <a onClick={() => this.setState({leftNav: !this.state.leftNav})}><i className="material-icons">menu</i></a>
+                </li>
+              </ul>
+              
+              
+            </div>
+          </nav>
+        </div>
+        <LeftNav open={this.state.leftNav}>
+          <MenuItem>Menu Item</MenuItem>
+        </LeftNav>
       </div>
     );
   }
